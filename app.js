7c50cea7dfb143473390d2d8219d69564e3f1a69
mis <18970065896@163.com>
2016-12-22 21:20:56 +0800

    a problem occured with DOM during echart render

---
+++ app.js
@@ -0,0 +1,19 @@
+// var koa = require('koa');
+// var app = koa();
+
+const Koa = require('koa');
+const app = new Koa();
+const bodyParser = require('koa-bodyparser');
+
+
+
+const controller = require('./controller');
+
+
+
+app.use(bodyParser());
+
+app.use(controller());
+
+app.listen(3000);
+console.log('app started at port 3000...');
